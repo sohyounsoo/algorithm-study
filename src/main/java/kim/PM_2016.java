799ba73f735d2560d7bcecdae8b921e745b8bb95
김KimGyeongHa <rudgkrk11@naver.com>
2024-04-16 00:06:34 +0900

    [1일차] 2016 김경하

---
+++ src/main/java/kim/PM_2016.java
@@ -0,0 +1,22 @@
+package kim;
+
+import java.util.Arrays;
+
+public class PM_2016 {
+
+    static String solution(int a, int b){
+        String[] days = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int[] monthDays = {31,29,31,30,31,30,31,31,30,31,30,31};
+        // 둠스데이 알고리즘, -1 은 배열 days를 맞추기 위함.
+        int totalDays = (Arrays.stream(monthDays).limit(a-1).sum() + b) - 1 ;
+
+        return days[totalDays%7];
+    }
+
+
+    public static void main(String[] args) {
+        int a = 5, b= 24;
+        System.out.println(solution(a,b));
+        System.out.println(176%7);
+    }
+}
