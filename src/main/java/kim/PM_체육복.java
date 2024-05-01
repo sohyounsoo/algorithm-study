dc8be2c4937376ec8f81bb3f3dcc9d20fcf41a6a
kimGyeongHa <rudgkrk11@naver.com>
2024-05-01 12:09:05 +0900

    fix: [6일차]체육복 수정

--- src/main/java/kim/PM_체육복.java
+++ src/main/java/kim/PM_체육복.java
@@ -1,50 +1,46 @@
 package kim;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class PM_체육복 {
-    static int COUNT = 0;
-
     static int solution(int n, int[] lost, int[] reserve) {
 
-        int[] lost_remove_reserve = Arrays.stream(reserve).filter(item -> {
-            for (int a : lost) {
-                if (item == a) {
-                    COUNT++;
+        int[] real_reserve = Arrays.stream(reserve).filter(item -> {
+            for(int i = 0 ; i < lost.length ; i++){
+                if(lost[i] == item) {
+                    lost[i] = -1;
                     return false;
                 }
             }
             return true;
-        }).toArray();
+        }).sorted().toArray();
 
-        Arrays.sort(lost_remove_reserve);
-        Arrays.sort(lost);
+        int[] real_lost = Arrays.stream(lost).filter(num -> num != -1).sorted().toArray();
 
-        int lost_val = (int)Arrays.stream(lost).filter(lostNum -> {
-            for (int i = 0 ; i < lost_remove_reserve.length ; i++) {
-                if(lost_remove_reserve[i] == 0) continue;
+        int count = 0;
 
-                int prev = lost_remove_reserve[i] - 1;
-                int next = lost_remove_reserve[i] + 1;
+        for(int real_lost_val : real_lost){
+            for(int i = 0 ; i < real_reserve.length ; i++){
+                if(real_reserve[i] == 0) continue;
 
-                if(lostNum == prev || lostNum == next) {
-                    lost_remove_reserve[i] = 0;
-                    return false;
+                int prev = real_reserve[i] - 1;
+                int next = real_reserve[i] + 1;
+
+                if(real_lost_val == prev || real_lost_val == next) {
+                    real_reserve[i] = 0;
+                    count++;
+                    break;
                 }
             }
-            return true;
-        }).count();
+        }
 
-        return  n - lost_val + COUNT;
+        return  n - (real_lost.length - count);
     }
 
     public static void main(String[] args) {
         int n = 5;
-        int[] lost = {1,2,3};
-        int[] reserve = {3,4};
+        int[] lost = {2,4};
+        int[] reserve = {3};
 
         System.out.println(solution(n, lost, reserve));
     }
