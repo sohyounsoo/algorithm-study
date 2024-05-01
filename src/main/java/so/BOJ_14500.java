0c657032a57e7fce61b7c3c73940f21a576ce407
sohyounsoo <sohyounsoo@gmail.com>
2024-05-01 21:34:21 +0900

    소수만들기

---
+++ src/main/java/so/BOJ_14500.java
@@ -0,0 +1,15 @@
+package so;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_14500 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+    }
+}
