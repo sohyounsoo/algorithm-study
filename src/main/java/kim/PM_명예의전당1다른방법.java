cb96749181d086978b7fab511218fd6301e3908a
kimGyeongHa <rudgkrk11@naver.com>
2024-05-11 11:29:16 +0900

    [13일차] 크레인인형뽑기 김경하

--- src/main/java/kim/PM_명예의전당1다른방법.java
+++ src/main/java/kim/PM_명예의전당1다른방법.java
@@ -1,7 +1,5 @@
 package kim;
 
-import com.sun.corba.se.impl.legacy.connection.USLPort;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
---
+++ src/main/java/kim/PM_크레인인형뽑기.java
@@ -0,0 +1,41 @@
+package kim;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class PM_크레인인형뽑기 {
+
+    static int solution(int[][] board, int[] moves){
+        int answer = 0;
+        List<Integer> machine = new ArrayList<>();
+
+        for (int move : moves){
+            for (int i = 0 ; i < board[move-1].length ; i++){
+                if(board[i][move-1] > 0){
+                    machine.add(board[i][move-1]);
+                    board[i][move-1] = -1;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 1 ; i < machine.size() ; i++ ) {
+            if (machine.get(i-1).equals(machine.get(i))) {
+                answer += 2;
+                machine.remove(i-1); machine.remove(i-1);
+                i = 0;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] board= {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 4, 4, 0}, {1, 2, 2, 1}};
+        int[] moves = {2, 3, 1, 4, 2, 3};
+
+        System.out.println(solution(board,moves));
+    }
+}
