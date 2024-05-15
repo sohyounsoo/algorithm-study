45924e95825fa574d657fd58524606b992391f66
sohyounsoo <sohyounsoo@gmail.com>
2024-05-15 18:53:48 +0900

    데이터분석 수정

--- src/main/java/so/programmers/PM_데이터_분석.java
+++ src/main/java/so/programmers/PM_데이터_분석.java
@@ -17,41 +17,24 @@ public class PM_데이터_분석 {
         solution(data, ext, val_ext, sort_by);
     }
 
-    public static int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        int[][] answer;
-        List<Integer> list = new ArrayList<>();
-        int index, sort;
+    public static List<int[]> solution(int[][] data, String ext, int val_ext, String sort_by) {
+        List<int[]> list = new ArrayList<>();
+        String[] s = {"code", "date", "maximum", "remain"};
+        int extIdx=0, sortIdx=0;
 
-        if(ext.equals("code")) index = 0;
-        else if(ext.equals("date")) index = 1;
-        else if(ext.equals("maximum")) index = 2;
-        else index = 3;
-
-        if(sort_by.equals("code")) sort = 0;
-        else if(sort_by.equals("date")) sort = 1;
-        else if(sort_by.equals("maximum")) sort = 2;
-        else sort = 3;
+        for(int i=0; i<4; i++) {
+            if(ext.equals(s[i])) extIdx = i;
+            if(sort_by.equals(s[i])) sortIdx = i;
+        }
 
         for(int i=0; i<data.length; i++) {
-            if(val_ext > data[i][index]) {
-                list.add(i);
-            }
-        }
-        answer = new int[list.size()][4];
-
-        int a = 0;
-        for(int i : list) {
-            for(int j=0; j<4; j++) {
-                answer[a][j] = data[i][j];
-            }
-            a++;
+            if(val_ext > data[i][extIdx]) list.add(data[i]);
         }
 
-        Arrays.sort(answer, (o1, o2) -> {
-            return o1[sort]-o2[sort];
-        });
+        final int si = sortIdx;
+        Collections.sort(list, (o1, o2) -> o1[si]-o2[si]);
 
-        return answer;
+        return list;
     }
 
 }
